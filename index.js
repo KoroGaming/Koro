a0982db138565c86828ac39c8f0f158087f46855
KoroGaming <46068946+KoroGaming@users.noreply.github.com>
2018-12-22 14:53:50 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,12 +9,20 @@ bot.on('ready', function () {
     bot.user.setActivity('rien').catch(console.error)
 });
 
-bot.on('guildMemberAdd', member => {
-    member.createDM().then(channel => {
-        return channel.send('Bienvenue sur le serveur UzoriaRP' + member.displayName)
-        console.log(`${member.displayName} à rejoind le serveur.`)
-    }).catch(console.error)
+bot.on('guildMemberAdd', member =>{
+    let embed = new Discord.RichEmbed()
+        .setDescription(':tada: **' + member.user.username + '** a rejoint ' + member.guild.name)
+        .setFooter('Nous sommes désormais ' + member.guild.memberCount)
+    member.guild.channels.get('ID DU CHANNEL').send(embed)
+    member.addRole('ID DU ROLE A AJOUTER AUTOMATIQUEMENT')
+ 
 });
+ 
+bot.on('guildMemberRemove', member =>{
+    let embed = new Discord.RichEmbed()
+        .setDescription(':cry: **' + member.user.username + '** a quitté ' + member.guild.name)
+        .setFooter('Nous sommes désormais ' + member.guild.memberCount)
+    member.guild.channels.get('ID DU CHANNEL').send(embed)
 
 const ban = require('./kick et ban/ban');
 
